5f3c021514463d040dc317fd4043b19b9fb83c7c
VirgilIw <imanuelambar11@gmail.com>
2025-09-09 11:57:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// buat folder
+// const fs = require("fs");
+
+import { buatFile } from "./buatMusic.js";
+import { buatFolder } from "./makefolder.js";
+import { pindahFile } from "./moveFile.js";
+
+buatFile();
+buatFolder();
+pindahFile;
